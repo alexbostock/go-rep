f7f7cc493f000eb40b9088ffaa1ca3e56e53ed40
alexbostock <alex@alexbostock.co.uk>
2018-09-23 12:08:03 +0100

    Build command line interface

--- gorep.go
+++ gorep.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 type StateMachine struct {
 	acceptingStates  map[int]bool
 	stateTransitions map[int]map[rune]int
@@ -18,4 +24,29 @@ func (sm StateMachine) Match(str string) (ans string, matchFound bool) {
 }
 
 func main() {
+	if len(os.Args) != 4 {
+		fmt.Println("Usage: gorep (test|match) <regex> <string>")
+		return
+	}
+
+	switch strings.ToLower(os.Args[1]) {
+	case "test":
+		if sm, err := Compile(os.Args[2]); err == nil {
+			fmt.Println(sm.Test(os.Args[3]))
+		} else {
+			fmt.Println("Invalid regex:", os.Args[2])
+		}
+	case "match":
+		if sm, err := Compile(os.Args[2]); err == nil {
+			if ans, ok := sm.Match(os.Args[3]); ok {
+				fmt.Println("Match found:", ans)
+			}
+			fmt.Println("No match found")
+		} else {
+			fmt.Println("Invalid regex:", os.Args[2])
+		}
+	default:
+		fmt.Println("Invalid option:", os.Args[1])
+		fmt.Println("Usage: gorep (test|match) <regex> <string>")
+	}
 }
